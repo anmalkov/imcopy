a11e38e23e29710348eb0ac7ddf220c034bb2606
Andrew Malkov <an.malkov@outlook.com>
2023-04-20 10:18:38 +0200

    Copy synchronously with stack

--- src/Micopy/Services/CopyService.cs
+++ src/Micopy/Services/CopyService.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 
 namespace Micopy.Services;
 
-public record FileConfiguration(
+public record FileItem(
     string FileName,
     string SourceFolder,
     string DestinationFolder
@@ -36,7 +36,39 @@ public class CopyService
 
     private void CopyDirectories(IEnumerable<FolderConfiguration> folders, IEnumerable<IgnorePatternConfiguration>? ignorePatterns)
     {
-        var files = new Stack<FileConfiguration>();
+        var foundFiles = GetFiles(folders, ignorePatterns);
+        var files = new Stack<FileItem>(foundFiles);
+
+        var filesCount = files.Count;
+        var filesCopied = 0;
+
+        var stopwatch = Stopwatch.StartNew();
+        while (files.Count > 0)
+        {
+            var file = files.Pop();
+            if (!Directory.Exists(file.DestinationFolder))
+            {
+                Directory.CreateDirectory(file.DestinationFolder);
+            }
+            CopyFile(file);
+            filesCopied++;
+            DisplayProgressBar(filesCopied, filesCount);
+        }
+        stopwatch.Stop();
+
+        console.WriteLine($"{Environment.NewLine}{filesCount} files copied in {stopwatch.Elapsed}");
+    }
+
+    private static void CopyFile(FileItem file)
+    {
+        var sourceFile = Path.Combine(file.SourceFolder, file.FileName);
+        var destinationFile = Path.Combine(file.DestinationFolder, file.FileName);
+        File.Copy(sourceFile, destinationFile, overwrite: true);
+    }
+
+    private IEnumerable<FileItem> GetFiles(IEnumerable<FolderConfiguration> folders, IEnumerable<IgnorePatternConfiguration>? ignorePatterns)
+    {
+        var files = new List<FileItem>();
         foreach (var folder in folders)
         {
             var matcher = new Matcher();
@@ -54,169 +86,19 @@ public class CopyService
             var directoryWrapper = new DirectoryInfoWrapper(dirInfo);
             var result = matcher.Execute(directoryWrapper);
 
-            var foundFiles = result.Files.Select(file => {
+            var directoryFiles = result.Files.Select(file => {
                 var relativeFolder = Path.GetDirectoryName(file.Path);
                 var sourceFolder = Path.Combine(folder.Source, relativeFolder);
                 var destinationFolder = Path.Combine(folder.Destination, relativeFolder);
                 var fileName = Path.GetFileName(file.Path);
-                return new FileConfiguration(fileName, sourceFolder, destinationFolder);
+                return new FileItem(fileName, sourceFolder, destinationFolder);
             });
 
-            foreach (var file in foundFiles)
-            {
-                files.Push(file);
-            }
+            files.AddRange(directoryFiles);
         }
 
-        var filesCount = files.Count;
-        var filesCopied = 0;
-        var stopwatch = Stopwatch.StartNew();
-        while (files.Count > 0)
-        {
-            var file = files.Pop();
-            if (!Directory.Exists(file.DestinationFolder))
-            {
-                Directory.CreateDirectory(file.DestinationFolder);
-            }
-            File.Copy(Path.Combine(file.SourceFolder, file.FileName), Path.Combine(file.DestinationFolder, file.FileName), overwrite: true);
-            filesCopied++;
-            DisplayProgressBar(filesCopied, filesCount);
-        }
-        stopwatch.Stop();
-        console.WriteLine($"{Environment.NewLine}{filesCount} files copied in {stopwatch.Elapsed}");
+        return files;
     }
-
-
-
-    public async Task CopyDirectoryAsync(string sourceFolder, string destinationFolder, int? parallelism)
-    {
-        if (parallelism.HasValue && parallelism.Value == 0)
-        {
-            CopyDirectory(sourceFolder, destinationFolder);
-            return;
-        }
-
-        if (!parallelism.HasValue)
-        {
-            parallelism = DefaultParallelism;
-        }
-
-        await CopyDirectoryAsync(sourceFolder, destinationFolder, parallelism.Value);
-    }
-
-    private async Task CopyDirectoryAsync(string sourceFolder, string destinationFolder, int parallelism)
-    {
-        var filesCount = await GetFilesCountForDirectoryAsync(sourceFolder, parallelism);
-        var filesCopied = 0;
-        //CopyFilesAsync(sourceFolder, destinationFolder, parallelism, ref filesCopied, filesCount);
-    }
-
-    private async Task<int> GetFilesCountForDirectoryAsync(string folderPath, int parallelism)
-    {
-        var subdirectories = Directory.GetDirectories(folderPath, "*", SearchOption.AllDirectories);
-
-        int totalFileCount = 0;
-
-        var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = parallelism };
-        await Task.Run(() =>
-        {
-            Parallel.ForEach(subdirectories, parallelOptions, subdirectory =>
-            {
-                var directoryFilesCount = Directory.GetFiles(subdirectory).Length;
-                Interlocked.Add(ref totalFileCount, directoryFilesCount);
-            });
-        });
-
-        return totalFileCount;
-    }
-
-    private void CopyDirectory(string sourceFolder, string destinationFolder)
-    {
-        var filesCount = GetFilesCountForDirectory(sourceFolder);
-        var filesCopied = 0;
-        CopyFiles(sourceFolder, destinationFolder, ref filesCopied, filesCount);
-    }
-
-    private void CopyDirectoryNew(string sourceFolder, string destinationFolder)
-    {
-        var directoryInfo = new DirectoryInfo(sourceFolder);
-        var files = directoryInfo.GetFiles("*.*", SearchOption.AllDirectories);
-        var filesCount = files.Length;
-        var filesCopied = 0;
-
-        foreach (var file in files)
-        {
-            var relativePath = file.FullName.Substring(sourceFolder.Length + 1);
-            var targetFilePath = Path.Combine(destinationFolder, relativePath);
-            var targetFileDirectory = Path.GetDirectoryName(targetFilePath);
-
-            if (!string.IsNullOrEmpty(targetFileDirectory) && !Directory.Exists(targetFileDirectory))
-            {
-                Directory.CreateDirectory(targetFileDirectory);
-            }
-
-            file.CopyTo(targetFilePath, overwrite: true);
-            filesCopied++;
-            DisplayProgressBar(filesCopied, filesCount);
-        }
-    }
-
-    private static int GetFilesCountForDirectory(string folderPath)
-    {
-        var directoryInfo = new DirectoryInfo(folderPath);
-        int count = directoryInfo.GetFiles("*.*", SearchOption.AllDirectories).Length;
-        return count;
-    }
-
-    private void CopyFiles(string sourceFolder, string destinationFolder, ref int filesCopied, int filesCount)
-    {
-        if (!Directory.Exists(destinationFolder))
-        {
-            Directory.CreateDirectory(destinationFolder);
-        }
-
-        var directoryInfo = new DirectoryInfo(sourceFolder);
-
-        foreach (var file in directoryInfo.GetFiles())
-        {
-            var destinationPath = Path.Combine(destinationFolder, file.Name);
-            file.CopyTo(destinationPath, overwrite: true);
-            filesCopied++;
-            DisplayProgressBar(filesCopied, filesCount);
-        }
-
-        foreach (var subDirectory in directoryInfo.GetDirectories())
-        {
-            var nextSourceFolder = Path.Combine(sourceFolder, subDirectory.Name);
-            var nextDestinationFolder = Path.Combine(destinationFolder, subDirectory.Name);
-            CopyFiles(nextSourceFolder, nextDestinationFolder, ref filesCopied, filesCount);
-        }
-    }
-
-    //private async Task CopyFilesAsync(string sourceFolder, string destinationFolder, int parallelism, ref int filesCopied, int filesCount)
-    //{
-    //    if (!Directory.Exists(destinationFolder))
-    //    {
-    //        Directory.CreateDirectory(destinationFolder);
-    //    }
-
-    //    var directoryInfo = new DirectoryInfo(sourceFolder);
-
-    //    foreach (var file in directoryInfo.GetFiles())
-    //    {
-    //        var destinationPath = Path.Combine(destinationFolder, file.Name);
-    //        file.CopyTo(destinationPath, overwrite: true);
-    //        filesCopied++;
-    //        DisplayProgressBar(filesCopied, filesCount);
-    //    }
-
-    //    foreach (var subDirectory in directoryInfo.GetDirectories())
-    //    {
-    //        var nextSourceFolder = Path.Combine(sourceFolder, subDirectory.Name);
-    //        var nextDestinationFolder = Path.Combine(destinationFolder, subDirectory.Name);
-    //        CopyFiles(nextSourceFolder, nextDestinationFolder, ref filesCopied, filesCount);
-    //    }
-    //}
 
     private void DisplayProgressBar(int currentValue, int maxValue, int barSize = 50)
     {
